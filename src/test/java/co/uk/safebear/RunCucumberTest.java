aabb0400a149d9b397dab95ad67f4cc9b8af5da2
simon stratton <student@safebear.co.uk>
2019-07-22 14:43:41 +0100

    Adding setup comments

--- src/test/java/co/uk/safebear/RunCucumberTest.java
+++ src/test/java/co/uk/safebear/RunCucumberTest.java
@@ -1,12 +1,16 @@
 package co.uk.safebear;
 
+// connecting the libraries that we imported to our code
+// might need to revisit these if we use a newer version of cucumber ie io.cucumber etc
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        // using json plug in to generate reports - to use with 3rd party tools
         plugin = {"json:target/cucumber-report/cucumber.json"},
+        // allow us to define which tests we want to run based on the tags we specify
         tags = "not @to-do"
         // glue = "path to Stepdefs if not in the same folder",
         // features = "pathto features if not in co.uk.safebear folder"
